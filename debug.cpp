bc49c8bda5fae09f8954fe277328eed5dcc18da6
Grace Herrera <gracebug1@gmail.com>
2017-09-01 13:34:29 -0600

    changed bitset to 32 bits and it didn't do what I wanted it to do -- going back to the drawing board

--- debug.cpp
+++ debug.cpp
@@ -14,19 +14,20 @@ int main(){
     {
 
       for (size_t i = 0; i < line.size(); ++i){
-        bitset<8> bitset1(line.c_str()[i]);
-        if (bitset1[7] == 0){
-          for(int k = 6; k >= 0; k--){cout << bitset1[k];}
-          cout << line << endl;
-        }
-        else if(bitset1[7] == 1 && bitset1[6] == 1 && bitset1[5] == 1){
-          cout << line <<" feed the stripped bitsets to an array" << endl;}
-        //cout << line << " " << bitset1[7] << endl;
-        //cout << line << " " << bitset1 << endl;
-        //cout << line << " " << bitset1[7] << endl;
-        // for(int j = 0; j < bitset1.size(); j++){
-        //   if (bitset1[0] == 0) {cout << "zero";}
+        bitset<32> bitset1(line.c_str()[i]);
+        cout << line << bitset1 << endl;
+        // if (bitset1[7] == 0){
+        //   for(int k = 6; k >= 0; k--){cout << bitset1[k];}
+        // //  cout << line << endl;
         // }
+        // else if(bitset1[7] == 1 && bitset1[6] == 1 && bitset1[5] == 1){
+        // }
+        // //cout << line << " " << bitset1[7] << endl;
+        // //cout << line << " " << bitset1 << endl;
+        // //cout << line << " " << bitset1[7] << endl;
+        // // for(int j = 0; j < bitset1.size(); j++){
+        // //   if (bitset1[0] == 0) {cout << "zero";}
+        // // }
       }
     }
     input.close();
